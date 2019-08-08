1a7d61e78d713d98c992f7537fe60d04a457e534
ccj_vm <1609792573@qq.com>
2019-08-08 15:05:28 +0800

    测试冲突？

---
+++ src/test/java/helloworld/helloworld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("heloworld7");
+		System.out.println("heloworld3");
+		System.out.println("测试冲突");
+	}
+}
